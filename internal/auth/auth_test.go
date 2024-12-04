171335e0900cbdea973dd380864ac90562b564a9
xsynch <jdstrickland@gmail.com>
2024-12-03 21:23:47 -0500

    add style tests with go fmt

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -7,34 +7,34 @@ import (
 	"testing"
 )
 
-func TestAuth(t *testing.T){
-	
-	req,_ := http.NewRequest("GET","http.example.com",nil)
+func TestAuth(t *testing.T) {
 
-	req.Header.Set("Authorization","ApiKey thisisatest")
-	
+	req, _ := http.NewRequest("GET", "http.example.com", nil)
+
+	req.Header.Set("Authorization", "ApiKey thisisatest")
+
 	expected := "thisisatest"
 	val, err := GetAPIKey(req.Header)
 	if err != nil {
-		fmt.Errorf("Error was returned: %s",err)
+		fmt.Errorf("Error was returned: %s", err)
 	}
 	if expected != val {
-		t.Errorf("unexpected reuturn: got %v want %v",val, expected)
+		t.Errorf("unexpected reuturn: got %v want %v", val, expected)
 	}
-	
+
 }
 
-func TestAuthFailure(t *testing.T){
-	
-	req,_ := http.NewRequest("GET","http.example.com",nil)
+func TestAuthFailure(t *testing.T) {
 
-	req.Header.Set("NoAuthorization","ApiKeythisisatest")
-	
+	req, _ := http.NewRequest("GET", "http.example.com", nil)
+
+	req.Header.Set("NoAuthorization", "ApiKeythisisatest")
+
 	expected := ErrNoAuthHeaderIncluded
 	val, err := GetAPIKey(req.Header)
 
 	if expected != err {
-		t.Errorf("unexpected reuturn: got %v want %v",val, expected)
+		t.Errorf("unexpected reuturn: got %v want %v", val, expected)
 	}
-	
+
 }
